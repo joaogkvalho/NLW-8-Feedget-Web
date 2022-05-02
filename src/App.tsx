5eb2dbb5a823d53368d2b098243a353034fec649
joaogkvalho <joaogkvalho@gmail.com>
2022-05-02 10:43:20 -0300

    :tada: project started

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps {
+  text?: string
+}
+
+function Button(props: ButtonProps){
+  return(
+    <button>{props.text ?? 'Default'}</button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  )
+}
+
+export default App
